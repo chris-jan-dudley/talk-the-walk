dfe8d167d4c1bf8c700197cdbcda998e2818f229
chris-jan-dudley <chris.jan.dudley@gmail.com>
2023-05-30 09:28:29 +0100

    route fix for logout

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
 end
